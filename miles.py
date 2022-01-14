24c7f6db5fd4afd92da6463fcd07621e1748ed58
gautama-bharadwaj <gautama98@gmail.com>
2022-01-13 23:06:13 -0800

    - Fixes

--- miles.py
+++ miles.py
@@ -8,15 +8,20 @@ config_data = json.load(f)
 block_distribution = config_data['blocks_walked'];
 
 proj_intersections = 10
-proj_volume = 423
+proj_volume = 10000
 
 if (len(block_distribution.keys()) > proj_intersections):
     block_distribution[str(proj_intersections)] = sum(
         list(block_distribution.values())[proj_intersections-1:])
-    print(block_distribution)
 
+for i in range(proj_intersections+1, int(list(block_distribution.keys())[-1])+1):
+    del block_distribution[str(i)]
+
+print(block_distribution)
 temp = 0
 for i in range(1, proj_intersections+1):
+    if (int(list(block_distribution.keys())[-1]) < i):
+        break
     temp += (block_distribution[str(i)]/100)*(i)
 
 people = math.floor(proj_volume/temp)
@@ -24,6 +29,8 @@ print("Unique people: " + str(people))
 
 distance = 0
 for i in range(1, proj_intersections+1):
+    if (int(list(block_distribution.keys())[-1]) < i):
+        break
     distance += people*(block_distribution[str(i)]/100)*(i*(1/9))
 
-print("Total distance travelled: " + str(distance) + " miles")
+print("Total distance travelled: " + str(round(distance)) + " miles")
